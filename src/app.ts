417990804412d53c6489a36bc7e2285283fc7921
Dan Elebash <danelebash@hotmail.com>
2015-09-04 00:32:04 -0400

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import {Router} from 'aurelia-router';
+import 'bootstrap';
+import 'src/css/styles.css!';
+
+export class App {
+  router:Router;
+
+  configureRouter(config, router:Router) {
+
+    config.title = 'Aurelia';
+    config.map([
+      {route: ['', 'main'], moduleId: './views/main', nav: false},
+      { route: 'expenses',  moduleId: './views/expenses', nav: true, title:'First' },
+      { route: 'receipts',  moduleId: './views/receipts', nav: true, title:'Second' },
+      { route: 'nothing',  moduleId: './views/nothing', nav: true, title:'Third' }
+    ]);
+
+    this.router = router;
+  }
+
+
+}
